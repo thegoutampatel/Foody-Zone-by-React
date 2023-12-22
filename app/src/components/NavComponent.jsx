e70dc2b03b9a90a335ca31294aa0c8f687b6f587
thegoutampatel <Goutampatel9131@gmail.com>
2023-12-22 17:07:34 +0530

    fix bugs and responsivness

--- app/src/components/NavComponent.jsx
+++ app/src/components/NavComponent.jsx
@@ -6,7 +6,7 @@ function NavComponent({searchFood, filterFood}) {
     <div className=" pt-[4vh] lg:pt-[8vh] pb-[3vh] ">
         <div className="flex lg:flex-row flex-col items-center lg:justify-around gap-2 lg:gap-0 ">
             <div>
-                <img src="public/Foody-Zone.svg" alt="Logo" />
+                <img src="public/logo.png" alt="Logo" width={200} />
             </div>
             <div>
                 <input onChange={searchFood} type="text"  placeholder="Search Food..." className="bg-transparent border-red-500 border rounded-sm pr-1 pl-0 lg:pr-2 lg:pl-1 text-white"/>
